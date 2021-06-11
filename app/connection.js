b8d9097f2076cfedfc6e9811c60bf899e303b72c
AllisonWonderland <aneary91@gmail.com>
2021-06-11 11:40:00 -0500

    created a new promise around mysql, still have to close/end it

--- app/connection.js
+++ app/connection.js
@@ -8,7 +8,15 @@ class Database {
 
   }
 
-  
+  query(sql, args = []) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args,(err, rows) => {
+        if (err) return reject(err);
+        resolve(rows)
+      })
+    })
+  }
+
 }
 
 
