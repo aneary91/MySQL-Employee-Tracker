5e8e58f58843448868ecb7c889eed5f9d6d43e73
AllisonWonderland <aneary91@gmail.com>
2021-06-11 12:35:25 -0500

    created the choice list

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const db = require("./db");
+const mysql = require("mysql");
 const consoleTable = require("console.table");
 const { title } = require("process")
 const db = require("./app/connection");
@@ -12,11 +12,22 @@ async function runPrompts() {
       name: "action",
       message: "What do you want to do today?",
       choices: [
-        'View all Employees'
-      ] 
-    
-    },
-    
+        'View all Employees',
+        'View all Employees by Department',
+        'View all Employees by Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Edit Employee Role',
+        'Edit Employee Manager',
+        'Remove Manager',
+        'View all Roles',
+        'Add Role',
+        'Remove Role',
+        'View all Departments',
+        'Add Department',
+        'Remove Department',
+      ], 
+    }
   ])
 
   .then(answers => {
