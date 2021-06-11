64d9e63c43de9baaf1f3493a8a3d65dcd67030f2
AllisonWonderland <aneary91@gmail.com>
2021-06-11 11:26:28 -0500

    moved connection.js into its own folder and changing the Database

--- roleData/roleDate.js
+++ roleData/roleDate.js
@@ -1,10 +1,9 @@
 const connection = require("../configuration/connection");
 
 function roleData() {
-  return connection.query(
+    return connection.query(
     "SELECT title, salary, id, department_id, department_name FROM roles JOIN department ON roles.department_id = department.deprtment_id"
-  );
+);
 }
 
-
-module.exports = roleData
+module.exports = roleData;
